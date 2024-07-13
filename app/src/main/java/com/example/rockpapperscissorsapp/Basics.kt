30a961faf87665b83be820c03b4645e764c1c5a9
DanDev_ <danuart21@gmail.com>
2024-07-13 12:43:16 +0800

    learn basic kotlin (print and data type)

---
+++ app/src/main/java/com/example/rockpapperscissorsapp/Basics.kt
@@ -0,0 +1,27 @@
+package com.example.rockpapperscissorsapp
+
+fun main() {
+
+//    val is a constant variable, so we can't reassignment the value (like const in javascript)
+    val num1 = 10
+//    num1 = 15
+
+//    var is a variable that we can reassignment the value (like let in javascript)
+    var num2 = 10
+    num2 = 15
+
+//    Byte, Int, and Long is datatype for numbers
+    val num3: Byte = 100
+//    var num3: Int = 100
+//    var num3: Long = 100
+
+//    Float and Double is a datatype for numbers that have coma, like 3.14
+    val num4: Float = 3.14f
+//    val num4: Double = 3.14
+
+
+
+
+
+    println("Hello World!")
+}
